7dcd4cb65d576d2f0de76bc85a5688d56c623e07
zwluoqi <qingzhu.us@gmail.com>
2021-11-20 11:44:01 +0800

    Initial commit

---
+++ Assets/_3th/General/SceneRenderPipelineConfig.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering;
+using UnityEngine.Rendering;
+
+[ExecuteInEditMode]
+public class SceneRenderPipelineConfig : MonoBehaviour
+{
+    public RenderPipelineAsset renderPipelineAsset;
+    public LightingSettings lightingSettingsAsset;
+    public Material skyBoxAsset;
+    void OnEnable()
+    {
+        GraphicsSettings.renderPipelineAsset = renderPipelineAsset;
+        Lightmapping.lightingSettings = lightingSettingsAsset;
+        RenderSettings.skybox = skyBoxAsset;
+    }
+
+    void OnValidate()
+    {
+        GraphicsSettings.renderPipelineAsset = renderPipelineAsset;
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# URP_Collection_Tech
+ 一些URP实践用例
